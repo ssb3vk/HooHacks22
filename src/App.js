9035e05cee813e38a0e3b6d1c4817b0346bbcff4
mootree7 <mag2sqv@virginia.edu>
2022-03-26 21:08:52 -0400

    netlify hosting testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() { 
+
+  async function fetchText() {
+    let response = await fetch('https://www.mathsisfun.com/algebra/index.html');
+    let data = await response.text();
+    console.log(data);
+  }
+  fetchText()
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
