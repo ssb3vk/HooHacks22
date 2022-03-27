782c77a35a5b2db5d5a917f91dde5aeb0a1c83df
Elijah Kin <3limkin@gmail.com>
2022-03-27 04:22:31 -0400

    Pass links

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3001;
 
 // URL for data
-const URL = "https://www.history.com/topics/japan/russo-japanese-war";
+// const URL = "https://www.history.com/topics/japan/russo-japanese-war";
 
 // Get raw data
 const getRawData = (url) => {
@@ -15,6 +15,13 @@ const getRawData = (url) => {
           return data;
        });
  };
+
+ // Return website body
+ const body = async (url) => {
+    const raw = await getRawData(url);
+    let $ = cheerio.load(raw);
+    return $('p').text();
+};
 
 // Return website title
 const title = async (url) => {
@@ -31,9 +38,10 @@ const fk = async (url) => {
     return 69; // change to use https://github.com/dana-ross/flesch-kincaid
 };
 
-app.get('/data', async (req, res) => {
-    const [t, s] = await Promise.all([title(URL), fk(URL)]);
-    res.json({title: t, fk: s, url: URL});
+app.get('/data/:link', async (req, res) => {
+    const URL = req.params.link.replaceAll("%2f", "/");
+    const [t, b, s] = await Promise.all([title(URL), body(URL), fk(URL)]);
+    res.json({title: t, fk: s, url: URL, body: b});
 });
 
 app.listen(PORT, function(err){
--- frontend/src/components/Page.js
+++ frontend/src/components/Page.js
@@ -6,16 +6,21 @@ function useScrapes(){
         title: "",
         url: "",
         fk: 0,
+        body: "",
     });
 
     useEffect(function (){
 
         (async () => {
             console.log("Mounting or updating");
-            const res = await fetch('/data');
+            const link1 = "https://www.history.com/topics/japan/russo-japanese-war";
+            const link2 = link1.replaceAll("/", "%2f")
+            const res = await fetch('/data/' + link2);
             // await console.log(res);
             const data = await res.json();
             console.log(data);
+
+
             setScrapes(data);
         } )();
 
